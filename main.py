eed7886e86a74c07cf0098410145aa1bf0eccac9
Mayra Amaral <mayracunha18@gmail.com>
2025-11-24 01:25:01 -0300

    adds module three

--- main.py
+++ main.py
@@ -1,20 +1,37 @@
 import pygame
 from constants import *
 from logger import log_state
+from player import *
 
 def main():
     print(f"Starting Asteroids with pygame version: {pygame.version.ver}")
     print(f"Screen width: {SCREEN_WIDTH}")
     print(f"Screen height: {SCREEN_HEIGHT}")
+    
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
     clock = pygame.time.Clock()
     dt = 0
+
+    updatable = pygame.sprite.Group()
+    drawable = pygame.sprite.Group()
+
+    Player.containers = (updatable, drawable)
+    player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
+
     while True:
-        log_state()
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
+        
         screen.fill("black")
+
+        for obj in drawable:
+            obj.draw(screen)
+
+        updatable.update(dt)
+
+        log_state()
+
         pygame.display.flip()
         dt = clock.tick(60) / 1000
 
